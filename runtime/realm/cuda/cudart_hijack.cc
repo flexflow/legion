ce1716efd135eb6e02b3e12fef00d540eff2fa66
Zhihao Jia <zhihao@cmu.edu>
2024-01-20 11:06:15 -0500

    remove deadcode

--- runtime/realm/cuda/cudart_hijack.cc
+++ runtime/realm/cuda/cudart_hijack.cc
@@ -778,10 +778,7 @@ extern "C" {
     p->gpu_memcpy_from_symbol_async(dst, src, size, offset, stream);
     return cudaSuccess;
   }
-   
-  //REALM_PUBLIC_API
-  //cudaError_t cudaDeviceEnablePeerAccess (int peerDevice, unsigned int flags) { return cudaSuccess; }
-   
+
   REALM_PUBLIC_API
   cudaError_t cudaDeviceSetSharedMemConfig(cudaSharedMemConfig config)
   {
